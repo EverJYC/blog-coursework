e715a4cb375bf2889a19322b9d90491116a054be
JXC1259 <JXC1259@student.bham.ac.uk>
2020-06-20 06:49:13 +0800

    shift comment page to to post page

--- blog/forms.py
+++ blog/forms.py
@@ -13,3 +13,15 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('author', 'text',)
+
+
+        widgets = {
+          'text': forms.Textarea(
+                                  attrs={'placeholder':"Write your comment",
+                                         'class':"comment-form-text"}
+                                 ),
+            'author': forms.TextInput(
+                attrs={'placeholder': "Name",
+                       'class':"comment-form-author"}
+            ),
+        }
--- blog/views.py
+++ blog/views.py
@@ -11,7 +11,20 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+
+
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    # return render(request, 'blog/add_comment_to_post.html', {'form': form})
+    return render(request, 'blog/post_detail.html', {'post': post, 'form': form})
 
 @login_required
 def post_new(request):
